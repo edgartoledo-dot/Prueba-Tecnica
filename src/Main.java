d5226ec112adc73b4fd98d361c528831fc5a1133
Edgar Fernando Toledo Moreno <edgtolmo@osigu.local>
2026-02-20 15:29:56 -0500

    Cambio del Main Segunda Version del proyecto

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import vehiculo.MotosFernando;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Nueva Version");
         int numero = 77777;
         String nombre = "FernandoToledo";
         char sexo = 'M';
